cc3b90f711d5c5fbdf270a4e4c665b1a8788db63
Soner <114054564+SonerA1@users.noreply.github.com>
2023-08-28 13:52:12 +0300

    eureka-server-updated

---
+++ EurekaServer/eureka-server/eureka-server/src/test/java/com/sonera1/stockmanagement/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonera1.stockmanagement.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
