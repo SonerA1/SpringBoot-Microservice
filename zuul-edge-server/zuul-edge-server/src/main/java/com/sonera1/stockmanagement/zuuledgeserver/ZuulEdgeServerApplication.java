442e2df928586cfde8adb649a136775ca4b87133
Soner <114054564+SonerA1@users.noreply.github.com>
2023-08-28 13:54:10 +0300

    zuul-edge-server-updated

--- zuul-edge-server/zuul-edge-server/src/main/java/com/sonera1/stockmanagement/zuuledgeserver/ZuulEdgeServerApplication.java
+++ zuul-edge-server/zuul-edge-server/src/main/java/com/sonera1/stockmanagement/zuuledgeserver/ZuulEdgeServerApplication.java
@@ -3,17 +3,15 @@ package com.sonera1.stockmanagement.zuuledgeserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableDiscoveryClient
-@EnableEurekaClient
 @SpringBootApplication
+@EnableZuulServer
 @EnableZuulProxy
-@CrossOrigin(origins = "*", maxAge = 3600)
-
-
+@CrossOrigin(origins = "*",maxAge = 3600)
 public class ZuulEdgeServerApplication {
 
     public static void main(String[] args) {
